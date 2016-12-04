853c1a213c35f3237c6ad03d00756df25293d4b5
abdoukercode <almath1106>
2016-12-04 20:50:42 +0100

    complete authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
        end
     end
     def destroy
-        session[:user_id] = "You have logged out"
+        session[:user_id] = nil
         flash[:success] = "You have logged out"
         redirect_to root_path
         
